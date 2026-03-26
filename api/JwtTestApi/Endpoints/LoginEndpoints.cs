fd41833deec918b6cdd3e24b922cb68a107f09a4
Jacek Papiniak <jacekpapiniak@gmail.com>
2026-03-26 21:23:49 +0000

    Initial Endpoint Login

---
+++ api/JwtTestApi/Endpoints/LoginEndpoints.cs
@@ -0,0 +1,24 @@
+﻿namespace JwtTestApi.Endpoints;
+
+public static class LoginEndpoints
+{
+    public static IEndpointRouteBuilder MapLoginEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/login", (LoginRequest request) =>
+        {
+            // In a real application, you would validate the user's credentials here
+            if (request.Username == "testuser" && request.Password == "password")
+            {
+                // Generate a JWT token (this is just a placeholder)
+                var token = "fake-jwt-token";
+                return Results.Ok(new { Token = token });
+            }
+            else
+            {
+                return Results.Unauthorized();
+            }
+        });
+
+        return app;
+    }
+}
