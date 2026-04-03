05d4f66508c435e40ddf01b2ab863144171c957c
Jacek Papiniak <jacekpapiniak@gmail.com>
2026-04-03 19:08:34 +0100

    Initial python work, start .NET test API from script

---
+++ checker/README.md
@@ -0,0 +1,5 @@
+﻿# How to run test .NET API Server
+To run the test .NET API Server, follow these steps:
+1. Open a terminal and navigate to the `checker/src/` directory.
+2. Run the following command to start the server: `python runTestApi.py`
+3. The server will start and listen for incoming requests. You can test the API endpoints using existing Bruno collections.
---
+++ checker/src/runTestApi.py
@@ -0,0 +1,67 @@
+﻿# This script file is used to spin up the test .NET API server that is used for testing this CLI tool.
+
+import subprocess # Used to run the .NET API server as a subprocess (dotnet run).
+import pathlib # Used to manipulate file paths in a cross-platform way (System.IO.Path in C#).
+import sys # Used to get the current working directory (System.Environment.CurrentDirectory in C#).
+
+# Find the root of the repository that contains checker and the test API server.
+# Uses the pathlib module to find the current file's path
+# Then iterates over parents until it finds the folder with the name of the repository (JWT-Endpoint-Checker).
+def find_repo_root():
+    # The name of the root folder, from there we have access to all the files and folders in project
+    top_folder_name = "JWT-Endpoint-Checker"
+
+    # Get the current working directory (the root of the project).
+    # https://docs.python.org/3/library/pathlib.html#basic-use
+    current_file_path = pathlib.Path(__file__).absolute()
+    print(f"Current file path: {current_file_path}")
+
+    # Traverse up the directory tree until we find the top folder.
+    for parent in current_file_path.parents:
+        if parent.name == top_folder_name:
+            print(f"Found repository root: {parent}")
+            return parent
+
+    print(f"Could not find repository root with name '{top_folder_name}' in the directory tree.")
+    return None # If we can't find the repository root, we return None. The caller should handle this case.
+
+def find_api_server():
+ root_path = find_repo_root()
+ if root_path is None:
+     print("Cannot find API server because repository root could not be found.")
+     return None
+
+ # Search for the JwtTestApi.csproj file in all subfolders starting from the root path.
+ # pathlib.Path.rglob() is searching for file in all directories but returns iterator,
+ # So the next() is needed to get the first match (there should only be one).
+ test_api_path = next(pathlib.Path(root_path).rglob("JwtTestApi.csproj"), None)
+ if test_api_path is None:
+     print("Could not find JwtTestApi.csproj file in the repository.")
+     return None
+
+ print(f"Found JwtTestApi.csproj file at: {test_api_path}")
+
+ return test_api_path
+
+def run_api_server(api_project_path):
+    print("Starting API server...")
+    if api_project_path is None:
+        print("Cannot run API server because the project path is None.")
+        return
+
+    # Verify if the .csproj file exists at the specified path.
+    if not api_project_path.is_file():
+        print(f"The specified .csproj file does not exist: {api_project_path}")
+        return
+
+    # Run the .NET API server using the dotnet CLI. This will block until the server is stopped.
+    print(f"Running API server from project path: {api_project_path}")
+    subprocess.run(["dotnet", "run", "--project", str(api_project_path)])
+
+def main():
+    server_path = find_api_server()
+    run_api_server(server_path)
+
+
+if __name__ == "__main__":
+    main()
