e1867f59b854ab6f8b43aa67ce0ccdab27198b5e
Jacek Papiniak <jacekpapiniak@gmail.com>
2026-04-17 21:20:04 +0100

    endpoint_analyser and validation result

--- checker/src/main.py
+++ checker/src/main.py
@@ -2,7 +2,10 @@
 from .cli.cli import build_cli # Import the argpars
 from .validators.parser_validator import validate_arguments # Import the function to validate command line arguments
 from .test_api import run_local_api_server, stop_api_server # Import the function to run the local API server
-from .jwt import token_loader # Import the token loader module to handle token loading from string, file, or URL
+from .jwt import token_loader, token_analyser # Import the token loader module to handle token loading from string, file, or URL
+from checker.src.jwt.token_analysis_result import TokenAnalysisResult # for defining the structure of the analysis result
+from checker.src.endpoint.endpoint_analyser import analyse_endpoint
+from checker.src.endpoint.endpoint_validation_result import EndpointValidationResult
 from datetime import datetime, timezone # for converting the exp claim to a human-readable format
 
 def main():
@@ -26,6 +29,10 @@ def main():
         stop_api_server()
         return
 
+    token : str
+    token_analysis_result : TokenAnalysisResult
+    endpoint_analysis_result : EndpointValidationResult
+
     if args.token:
         print("Loading token...")
         # The credentials are in format "email,password".
@@ -40,6 +47,19 @@ def main():
         token_analysis_result = token_analyser.analyse_token(token, int(datetime.now(timezone.utc).timestamp()))
         print("Token analysis result:")
         print(token_analysis_result)
+
+    if args.endpoint:
+        print("Testing endpoint...")
+        # We need to test the endpoint with the JWT token that we loaded.
+        # The endpoint should be provided as a URL, for example: http://localhost:5000/api/profile.
+        # We will use the token_analysis_result to get the token and test the endpoint with it.
+        # The result of the endpoint testing will be saved in the report.txt file.
+        # The report will contain the results of:
+        # - JWT token validation: Information about the validity of the token, its structure, and claims.
+        # - Endpoint testing: Information about the accessibility of the endpoint, the response status code.
+        endpoint_analysis_result = analyse_endpoint(args.endpoint, token)
+        print("Endpoint analysis result:")
+        print(endpoint_analysis_result)
     else:
         print("No action specified. Use -h or --help for usage information.")
 
